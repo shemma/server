60f73f9af38bba30b2db61be2be45517caf8bf1b
Shem Maimon <maimon.shem@gmail.com>
2018-05-12 22:09:32 +0200

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "Hello " });
+  res.send({ hi: "test" });
 });
 
 const PORT = process.env.PORT || 5000;
