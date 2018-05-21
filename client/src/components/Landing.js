14c5da46697cd783e92b01a320f158853219ee52
Shem Maimon <maimon.shem@gmail.com>
2018-05-21 11:55:27 +0200

    final test

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,7 +3,7 @@ import React from "react";
 const Landing = () => {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Emaily11!</h1>
+      <h1>Emaily!</h1>
       Collect feedback from your users
     </div>
   );
