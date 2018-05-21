5537f78e19c09d74f3ebf53ee8153ee6731395a3
Shem Maimon <maimon.shem@gmail.com>
2018-05-21 09:28:42 +0200

    Added Client and Billing

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,11 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
-import logger from "redux-logger";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(reduxThunk, logger)
-);
+const store = createStore(reducers, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
