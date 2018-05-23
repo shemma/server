0f7fad16b9df8bf5bc8f492e36e757ff06ac2c7b
Shem Maimon <maimon.shem@gmail.com>
2018-05-23 02:15:26 +0200

    initial Form

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,11 @@ import reducers from "./reducers";
 import axios from "axios";
 window.axios = axios;
 
-const store = createStore(reducers, applyMiddleware(reduxThunk));
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
